307d334bb88039f5c6ca006d76d25e98bdcd2188
Dhruv <dap332@drexel.edu>
2018-08-15 11:30:28 -0400

    works good now

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ const async = require('async');
 let newsData = [];
 
 const URI = 'localhost';
+const PORT = 5000;
 
 fs.readFile('./newsdata.csv', 'utf-8', (err, data) => {
    if (err) throw err;
    console.log('file loaded');
    let dataArr = data.split("\n");
    newsData = dataArr.splice(-dataArr.length - 1);
- //  post(newsData);
- http_post(JSON.stringify({"texts": ["this is good"]})).then( data => console.log(data));
+   post(newsData);
+ //http_post(JSON.stringify({"texts": ["this is good"]})).then( data => console.log(data));
 });
 
 
 function post(arr){
 	console.log(arr);
-   async.eachLimit(arr, 70, async(headline) => {
+   async.eachLimit(arr, 1000, async(headline) => {
 	let title = [];
 	title.push(headline.split(',')[1]);
 	let postData = JSON.stringify({"texts": title});
 	let text = await http_post(postData);
-	console.log(text);   
+	console.log(title[0], text);   
    }, (err) => {
       if (err) throw err;
    } );
@@ -35,7 +36,7 @@ function post(arr){
 function http_post(data) {
     const options = {
         hostname: URI,
-        port: 5000,
+        port: PORT,
         path: '/predict',
         method: 'POST',
         headers: {
@@ -45,12 +46,13 @@ function http_post(data) {
 
 
     return new Promise( (resolve, reject) => {
-		console.log('posting...');
+		//let headline = JSON.parse(data).texts[0];
        const req = http.request(options, (res) => {
-          res.on('data', (d) => (resolve(d)));
+          res.on('data', (d) => (resolve( d.toString('utf-8'))));
        });
       req.on('error', (e) => reject(e));
-    });
-      req.write(data);
+	  req.write(data);
       req.end();
+    });
+      
 }
