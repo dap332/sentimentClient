42513f9aabec9016eb4009e2a6f9cc4f5b7e6389
Dhruv <dap332@drexel.edu>
2018-08-15 10:48:12 -0400

    Init

---
+++ index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const fs = require('fs');
+const http = require('http');
+const async = require('async');
+let newsData = [];
+
+const URI = 'localhost';
+
+fs.readFile('./newsdata.csv', 'utf-8', (err, data) => {
+   if (err) throw err;
+   console.log('file loaded');
+   let dataArr = data.split("\n");
+   newsData = dataArr.splice(-dataArr.length - 1);
+ //  post(newsData);
+ http_post(JSON.stringify({"texts": ["this is good"]})).then( data => console.log(data));
+});
+
+
+function post(arr){
+	console.log(arr);
+   async.eachLimit(arr, 70, async(headline) => {
+	let title = [];
+	title.push(headline.split(',')[1]);
+	let postData = JSON.stringify({"texts": title});
+	let text = await http_post(postData);
+	console.log(text);   
+   }, (err) => {
+      if (err) throw err;
+   } );
+}
+
+
+
+function http_post(data) {
+    const options = {
+        hostname: URI,
+        port: 5000,
+        path: '/predict',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+
+    return new Promise( (resolve, reject) => {
+		console.log('posting...');
+       const req = http.request(options, (res) => {
+          res.on('data', (d) => (resolve(d)));
+       });
+      req.on('error', (e) => reject(e));
+    });
+      req.write(data);
+      req.end();
+}
